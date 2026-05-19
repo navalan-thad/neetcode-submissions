c45ca300d3a84701ce1d2e1a39a45eef951877df
Navalan Thad <85454400+navalan-thad@users.noreply.github.com>
2026-05-19 10:09:47 -0400

    Add: jump-game - submission-8

---
+++ Data Structures & Algorithms/jump-game/submission-8.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+
+        target = len(nums) - 1
+        memo = {}
+
+        def dp(posn):
+            if posn in memo:
+                return memo[posn]
+
+            if posn + nums[posn] >= target:
+                return True
+
+            maxJump = posn + nums[posn]
+
+            for dist in range(maxJump, posn, -1):
+                if dp(dist):
+                    memo[dist] = True
+                    return True
+
+            memo[posn] = False
+            return False
+
+        return dp(0)
+
+            
+
+
+        
