5198c050566daafb60755348326440ab2558f205
Navalan Thad <85454400+navalan-thad@users.noreply.github.com>
2026-06-19 09:57:14 -0400

    Add: insert-greatest-common-divisors-in-linked-list - submission-1

---
+++ Data Structures & Algorithms/insert-greatest-common-divisors-in-linked-list/submission-1.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+import math
+class Solution:
+    def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        curr = head
+        while curr.next:
+            temp = curr.next
+            node = ListNode(math.gcd(curr.val, temp.val))
+            curr.next = node
+            node.next = temp
+            curr = temp
+
+        return head
+        
