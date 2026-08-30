ea7f785454273a602708eed5cce6c21704ccf48a
Navalan Thad <85454400+navalan-thad@users.noreply.github.com>
2026-08-30 13:11:55 -0400

    Add: online-stock-span - submission-0

---
+++ Data Structures & Algorithms/online-stock-span/submission-0.py
@@ -0,0 +1,15 @@
+class StockSpanner:
+
+    def __init__(self):
+        self.prices = []
+
+    def next(self, price: int) -> int:
+        
+        span = 1
+
+        while self.prices and self.prices[-1][0] <= price:
+            span += self.prices[-1][1]
+            self.prices.pop()
+
+        self.prices.append((price, span))
+        return span
