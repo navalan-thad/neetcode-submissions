1a547207bb47cf8fc5897d58c9a114a5b56dce17
Navalan Thad <85454400+navalan-thad@users.noreply.github.com>
2026-09-11 15:38:17 -0400

    Add: reorganize-string - submission-0

---
+++ Data Structures & Algorithms/reorganize-string/submission-0.py
@@ -0,0 +1,39 @@
+import heapq
+
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+
+        freq = {}
+        for char in s:
+            freq[char] = freq.get(char, 0) + 1
+
+        if max(freq.values()) > (len(s)+1) // 2:
+            return ''
+
+        max_heap = [[-v, k] for k, v in freq.items()]
+
+        timeout = None
+        result = []
+
+        while max_heap or timeout:
+            if timeout and not max_heap: # only char left is on timeout
+                return ''
+
+            count, char = heapq.heappop(max_heap)
+            result.append(char)
+            count += 1 # decrementing negative freq
+
+            if timeout:
+                heapq.heappush(max_heap, timeout)
+                timeout = None
+
+            if count < 0:
+                timeout = [count, char]
+
+        return ''.join(result)
+                
+
+
+
+
+        
