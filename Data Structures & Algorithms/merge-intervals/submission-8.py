aa7fe699103c61f0bd3c7fcd7a6e20fe526d3164
Navalan Thad <85454400+navalan-thad@users.noreply.github.com>
2026-09-24 13:54:07 -0400

    Add: merge-intervals - submission-8

---
+++ Data Structures & Algorithms/merge-intervals/submission-8.py
@@ -0,0 +1,28 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+
+        # import heapq
+
+        intervals.sort(key=lambda x: x[0])
+        curr_end = intervals[0][1]
+        merged = [[intervals[0][0], curr_end]]
+
+        if len(intervals) == 1:
+            return merged
+
+        for i in range(1, len(intervals)):
+            start, end = intervals[i][0], intervals[i][1]
+
+            if start <= curr_end:
+                merged[-1][1] = max(end, curr_end)
+            else:
+                merged.append([start, end])
+                
+            curr_end = max(end, curr_end)
+
+        return merged
+
+
+
+
+        
